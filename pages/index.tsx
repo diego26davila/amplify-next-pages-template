bd89c1422a0a13a20dfa6723a5826e19c7511198
Catolica <Catolica@C073010.pucp.edu.pe>
2024-06-02 01:11:05 -0500

    add sign out button

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,6 +52,7 @@ export default function App() {
             Review next steps of this tutorial.
           </a>
         </div>
+        <button onClick={signOut}>Sign out</button>
       </main>
 
         )
